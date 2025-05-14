6a323c72777fb0208c127b28a0f3ae11370ad0df
Zeynel İTKÜ <zeynelsamsun55@gmail.com>
2025-05-14 10:19:56 +0300

    push denemesi

--- src/main/java/com/softwarearchitecture/QuickBook/Controller/PageController.java
+++ src/main/java/com/softwarearchitecture/QuickBook/Controller/PageController.java
@@ -1,11 +1,22 @@
 package com.softwarearchitecture.QuickBook.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
+@RequestMapping("/")
 public class PageController {
+    
+    @GetMapping
+    public String showIndexPage() {
+        return "index";
+    }
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "/about";
+    }
     @GetMapping("/login")
     public String showLoginPage() {
         return "login"; // templates/login.html
