8e49db33270bb9d864c4f2d2200287c97a5a26a6
Remzi <117019568+GmaSterAX@users.noreply.github.com>
2025-05-27 20:29:54 +0300

    Add files via upload

---
+++ src/main/java/com/softwarearchitecture/QuickBook/Model/Activity.java
@@ -0,0 +1,31 @@
+package com.softwarearchitecture.QuickBook.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotelId")
+    private Hotel hotel;
+
+}
+
