e753beecac868d7d382d91e071ac67c7c30adccf
Zeynel İTKÜ <zeynelsamsun55@gmail.com>
2025-06-09 12:30:22 +0300

    gereksiz dosyalar silindi

--- src/main/java/com/softwarearchitecture/QuickBook/Controller/NotificationController.java
+++ src/main/java/com/softwarearchitecture/QuickBook/Controller/NotificationController.java
@@ -3,22 +3,30 @@ package com.softwarearchitecture.QuickBook.Controller;
 
 import com.softwarearchitecture.QuickBook.Dto.NotificationDto;
 import com.softwarearchitecture.QuickBook.Service.NotificationService;
+import com.softwarearchitecture.QuickBook.Service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/notifications")
+@Controller
 public class NotificationController {
 
     private final NotificationService notificationService;
+    private final UserService userService;
 
     @Autowired
-    public NotificationController(NotificationService notificationService){
+    public NotificationController(NotificationService notificationService,
+                                  UserService userService){
         this.notificationService = notificationService;
+        this.userService = userService;
     }
 
     @GetMapping("/get/by/id/{id}")
@@ -43,5 +51,15 @@ public class NotificationController {
     public ResponseEntity<Void> deleteNotification(@PathVariable long notificationId) {
         notificationService.deleteById(notificationId);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/notifications")
+    public String getUserNotifications(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userMail = authentication.getName();
+        Long userId = userService.getUserByMail(userMail).getId();
+        List<NotificationDto> notifications = notificationService.getNotificationsByUserId(userId);
+        model.addAttribute("notifications", notifications);
+        return "notifications";
     }
 }
